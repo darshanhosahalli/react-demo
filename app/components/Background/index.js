d53e2733c375a827520c400b976d7bdb8532efe7
Darshan B.S <darshan.b.s@tadigital.com>
2021-03-30 11:29:35 +0530

    completed models

---
+++ app/components/Background/index.js
@@ -0,0 +1,20 @@
+/**
+ *
+ * Background
+ *
+ */
+
+import React, { memo } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+function Background() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+Background.propTypes = {};
+
+export default memo(Background);
